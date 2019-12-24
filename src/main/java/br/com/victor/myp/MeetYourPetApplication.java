8694db82098ee97ea85c8a0dc624265af79f3d30
Victor Luã <56847759+victor212luan@users.noreply.github.com>
2019-12-23 23:42:31 -0300

    Estrutura do projeto

---
+++ src/main/java/br/com/victor/myp/MeetYourPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.myp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetYourPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetYourPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victor/myp/MeetYourPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor.myp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetYourPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
