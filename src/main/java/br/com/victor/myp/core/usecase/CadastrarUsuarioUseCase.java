f0575787dde3d1f8a539523f112b58f6a477c871
Victor Luã <56847759+victor212luan@users.noreply.github.com>
2020-01-26 18:03:51 -0300

    TDD CadastrarUsuarioUseCase

---
+++ src/main/java/br/com/victor/myp/core/usecase/CadastrarUsuarioUseCase.java
@@ -0,0 +1,20 @@
+package br.com.victor.myp.core.usecase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.victor.myp.core.entity.UsuarioEntity;
+import br.com.victor.myp.core.gateway.UsuarioGateway;
+
+@Component
+public class CadastrarUsuarioUseCase {
+
+	@Autowired
+	private UsuarioGateway usuarioGateway;
+	
+	public UsuarioEntity cadastrarUsuario(UsuarioEntity entity, String pathImagem) {	
+		entity.setImagemUsuario(pathImagem);
+		
+		return usuarioGateway.cadastrarUsuario(entity);
+	}
+}
