05dc852ef0f65ab01cc2151f3c54c160edf32827
victor212luan <streamyodin@gmail.com>
2020-04-18 06:42:33 -0300

    (Fix) classes e mapper entrypoint

--- src/main/java/br/com/victor/myp/entrypoint/mapper/EnderecoMapper.java
+++ src/main/java/br/com/victor/myp/entrypoint/mapper/EnderecoMapper.java
@@ -9,6 +9,7 @@ import br.com.victor.myp.entrypoint.entity.EnderecoHttpModel;
 
 public class EnderecoMapper {
 
+	//TODO Mapear corretamente a cidade e estado
 	public static EnderecoEntity to(EnderecoHttpModel httpModel) {
 		return Optional.ofNullable(httpModel).map(e -> new EnderecoEntity(
 			e.getId(), e.getLogradouro(), e.getNumero(), e.getComplemento(), e.getCep(),
@@ -19,7 +20,7 @@ public class EnderecoMapper {
 	public static EnderecoHttpModel from(EnderecoEntity entity) {
 		return Optional.ofNullable(entity).map(e -> new EnderecoHttpModel(
 			e.getId(), e.getLogradouro(), e.getNumero(), e.getComplemento(), e.getCep(),
-			e.getBairro(), CidadeMapper.from(e.getCidade())))
+			e.getBairro(), e.getCidade().getNome(), e.getCidade().getEstado().getNome()))
 			.orElse(new EnderecoHttpModel());
 	}
 }
